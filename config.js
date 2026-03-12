cb5491a1eb333976fc5d92c7a905687bb2c77f8c
Superplayz yt <kavionsmith07@gmail.com>
2026-03-12 14:21:24 -0400

    Change YouTube URL in config.js
    Updated YouTube URL in the status configuration.

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     rotateDefault: [
       { name: 'Netflix', type: ActivityType.Watching },
       { name: 'GTA VI', type: ActivityType.Playing },
-      { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.twitch.tv/glaceytt' },
+      { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.youtube.com/@TheRealSuperEditz' },
       { name: 'Spotify', type: ActivityType.Custom },
     ],
     songStatus: true
@@ -14,3 +14,4 @@ module.exports = {
   spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
 }
+
